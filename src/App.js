92697336ede141b449ca715893d1120f054ecbbd
RileyManda <rileymanda69@gmail.com>
2023-07-31 11:18:34 +0200

    Implemented missions component

--- src/App.js
+++ src/App.js
@@ -1,29 +1,36 @@
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import logo from './logo.svg';
+import {
+  BrowserRouter, Routes, Route, Outlet,
+} from 'react-router-dom';
 import './App.css';
+
+function Rockets() {
+  return <div>Rockets Component</div>;
+}
+
+function Missions() {
+  return (
+    <div>
+      Missions Component
+      <Outlet />
+    </div>
+  );
+}
+
+function NotFound() {
+  return <div>If page not found it goes here</div>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Rockets />}>
+          <Route path="missions" element={<Missions />}>
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
