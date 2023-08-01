e28467f9821c008ccea872b10396c9df33cf2ee9
RileyManda <rileymanda69@gmail.com>
2023-08-01 14:49:00 +0200

    Code cleanup

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchMissions } from '../redux/missions/missionsSlice';
 
@@ -7,11 +7,10 @@ const Missions = () => {
   const missions = useSelector((state) => state.missions.missions);
   const isLoading = useSelector((state) => state.missions.isLoading);
   const error = useSelector((state) => state.missions.error);
-  const [fetchCount, setFetchCount] = useState(0); // testing the number of times data is fectehd
+
   useEffect(() => {
     console.log('Dispatching fetchMissions...');
     dispatch(fetchMissions());
-    setFetchCount((prevCount) => prevCount + 1);
   }, [dispatch]);
 
   if (isLoading) {
@@ -30,11 +29,6 @@ const Missions = () => {
 
   return (
     <div>
-      <div>
-        Number of times data fetched:
-        {' '}
-        {fetchCount}
-      </div>
       {missions.map((mission) => (
         <div key={mission.mission_id}>
           <h2>{mission.mission_name}</h2>
