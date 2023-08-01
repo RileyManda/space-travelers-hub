32b419ca79a29db395e412285847d1366e8b1334
RileyManda <rileymanda69@gmail.com>
2023-08-01 14:34:57 +0200

    Implemented fetchdata|mision data|redux reducers and actions

--- src/redux/missions/missionsSlice.js
+++ src/redux/missions/missionsSlice.js
@@ -1,15 +1,48 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
-const initialState = [];
+// Create an async thunk for fetching missions
+export const fetchMissions = createAsyncThunk(
+  'missions/fetchMissions',
+  async () => {
+    try {
+      const response = await axios.get('https://api.spacexdata.com/v3/missions');
+      return response.data;
+    } catch (error) {
+      throw new Error('Failed to fetch missions.');
+    }
+  },
+);
+
+const initialState = {
+  missions: [],
+  isLoading: false,
+  error: undefined,
+};
 
 const missionsSlice = createSlice({
   name: 'missions',
   initialState,
   reducers: {
-    showContent: () => {
-      const displayText = 'Missions states will update here';
-      return [displayText];
+    showContent: (state) => {
+      const displayText = 'Missions states';
+      state.missions = displayText;
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchMissions.pending, (state) => {
+        state.isLoading = true;
+        state.error = undefined;
+      })
+      .addCase(fetchMissions.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.missions = action.payload;
+      })
+      .addCase(fetchMissions.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.error.message;
+      });
   },
 });
 
