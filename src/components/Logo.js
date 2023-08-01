7cd769c28d2796e67f24fe53b2a13708f44a8576
RileyManda <rileymanda69@gmail.com>
2023-08-01 08:34:17 +0200

    Created logo component and added logo to header

---
+++ src/components/Logo.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import logo from '../assets/logo/app-logo.png';
+
+const Logo = () => <img src={logo} alt="App Logo" className="app-logo" />;
+
+export default Logo;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Logo from './Logo';
 
 const Navbar = () => (
   <nav>
+    <div className="logo-container">
+      <Logo />
+    </div>
     <ul>
       <li>
         <Link to="/">Rockets</Link>
