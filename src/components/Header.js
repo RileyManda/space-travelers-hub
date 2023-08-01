8d2c4cde05b86fb091d5c45b68c8d3d8cfb0c547
RileyManda <rileymanda69@gmail.com>
2023-08-01 10:13:28 +0200

    Created empty Profile component

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Logo from './Logo';
+
+const Header = () => (
+  <nav>
+    <div className="logo-container">
+      <Logo />
+    </div>
+    <ul>
+      <li>
+        <Link to="/">Rockets</Link>
+      </li>
+      <li>
+        <Link to="/missions">Missions</Link>
+      </li>
+    </ul>
+  </nav>
+);
+
+export default Header;
---
+++ src/components/Profile.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const Profile = () => (
+  <div>MyProfile</div>
+);
+export default Profile;
