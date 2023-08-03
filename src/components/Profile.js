a100f9c7d9bfb04703fd6374da4d9a8fc7c5ee8e
yaredtt <yaredtekle22@gmail.com>
2023-08-04 00:13:21 +0300

    Add rockets profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -5,7 +5,8 @@ import Table from 'react-bootstrap/Table';
 const Profile = () => {
   const missions = useSelector((state) => state.missions.missions);
   const joinedMissionsData = missions.filter((mission) => mission.reserved);
-
+  const rockets = useSelector((state) => state.rockets.rockets);
+  const reservedRockets = rockets.filter((rocket) => rocket.reserved);
   return (
     <div className="profile-container">
       <h2 className="mission-title">My Missions</h2>
@@ -24,8 +25,23 @@ const Profile = () => {
           <p>No joined missions yet.</p>
         )}
       </div>
+      <h2 className="rocket-title">My Rockets</h2>
+      <div className="reserved-rockets-container">
+        {reservedRockets.length > 0 ? (
+          <Table bordered>
+            <tbody>
+              {reservedRockets.map((rocket) => (
+                <tr key={rocket.id}>
+                  <td>{rocket.name}</td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        ) : (
+          <p className="empty-rocket">No reserved rockets yet.</p>
+        )}
+      </div>
     </div>
   );
 };
-
 export default Profile;
