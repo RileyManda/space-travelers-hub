bc40a9104681d718b15d49b95df1faae0c26e4b3
RileyManda <rileymanda69@gmail.com>
2023-08-03 18:02:14 +0200

    Implemented mission tests-loading state

---
+++ src/test/missions.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+import Missions from '../components/Missions';
+// import { joinMission, leaveMission } from '../redux/missions/missionsSlice';
+
+const mockStore = configureStore([]);
+
+test('renders loading text when data is loading', () => {
+  const store = mockStore({
+    missions: { missions: [], isLoading: true, error: null },
+  });
+
+  render(
+    <Provider store={store}>
+      <Missions />
+    </Provider>,
+  );
+
+  const loadingElement = screen.getByText(/Loading.../i);
+  expect(loadingElement).toBeInTheDocument();
+});
--- src/test/profile.test.js
+++ src/test/profile.test.js
@@ -1,52 +1,126 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
-import Profile from '../components/Profile';
-// import missionsReducer from '../redux/missions/missionsSlice';
+import Missions from '../components/Missions';
 
 const mockStore = configureStore([]);
 
-test('renders "My Missions" title and joined missions', () => {
-  const missions = [
-    { mission_id: 1, mission_name: 'Mission 1', reserved: true },
-    { mission_id: 2, mission_name: 'Mission 2', reserved: true },
-  ];
-
-  const store = mockStore({ missions: { missions } });
+test('renders loading message when missions are not available', () => {
+  const store = mockStore({
+    missions: { missions: [], isLoading: true, error: undefined },
+  });
 
   render(
     <Provider store={store}>
-      <Profile />
+      <Missions />
     </Provider>,
   );
 
-  const titleElement = screen.getByText(/My Missions/i);
+  const loadingElement = screen.getByText(/Loading.../i);
+  expect(loadingElement).toBeInTheDocument();
+});
+
+test('renders error message when there is an error fetching missions', () => {
+  const errorMessage = 'Failed to fetch missions.';
+  const store = mockStore({
+    missions: { missions: [], isLoading: false, error: errorMessage },
+  });
+
+  render(
+    <Provider store={store}>
+      <Missions />
+    </Provider>,
+  );
+
+  const errorElement = screen.getByText(new RegExp(errorMessage, 'i'));
+  expect(errorElement).toBeInTheDocument();
+});
+
+test('renders missions table with correct data when missions are available', () => {
+  const missions = [
+    {
+      mission_id: 1, mission_name: 'Mission 1', description: 'Mission 1 Description', reserved: true,
+    },
+    {
+      mission_id: 2, mission_name: 'Mission 2', description: 'Mission 2 Description', reserved: false,
+    },
+  ];
+
+  const store = mockStore({
+    missions: { missions, isLoading: false, error: undefined },
+  });
+
+  render(
+    <Provider store={store}>
+      <Missions />
+    </Provider>,
+  );
+
+  const titleElement = screen.getByText(/Mission/i);
   expect(titleElement).toBeInTheDocument();
 
   missions.forEach((mission) => {
-    const missionElement = screen.getByText(mission.mission_name);
-    expect(missionElement).toBeInTheDocument();
+    const missionNameElement = screen.getByText(mission.mission_name);
+    expect(missionNameElement).toBeInTheDocument();
+
+    const missionDescriptionElement = screen.getByText(mission.description);
+    expect(missionDescriptionElement).toBeInTheDocument();
+
+    const statusElement = screen.getByText(
+      mission.reserved ? /Active Member/i : /NOT A MEMBER/i,
+    );
+    expect(statusElement).toBeInTheDocument();
+
+    const buttonElement = screen.getByText(
+      mission.reserved ? /Leave Mission/i : /Join Mission/i,
+    );
+    expect(buttonElement).toBeInTheDocument();
   });
 });
 
-test('renders "No joined missions yet." when no missions are reserved', () => {
+test('clicking on "Join Mission" button should dispatch joinMission action', () => {
   const missions = [
     { mission_id: 1, mission_name: 'Mission 1', reserved: false },
-    { mission_id: 2, mission_name: 'Mission 2', reserved: false },
   ];
 
-  const store = mockStore({ missions: { missions } });
+  const store = mockStore({
+    missions: { missions, isLoading: false, error: undefined },
+  });
 
   render(
     <Provider store={store}>
-      <Profile />
+      <Missions />
     </Provider>,
   );
 
-  const titleElement = screen.getByText(/My Missions/i);
-  expect(titleElement).toBeInTheDocument();
+  const joinButtonElement = screen.getByText(/Join Mission/i);
+  fireEvent.click(joinButtonElement);
 
-  const noMissionsElement = screen.getByText(/No joined missions yet./i);
-  expect(noMissionsElement).toBeInTheDocument();
+  expect(store.getActions()).toEqual([
+    { type: 'missions/joinMission/pending' },
+  ]);
+});
+
+test('clicking on "Leave Mission" button should dispatch leaveMission action', () => {
+  const missions = [
+    { mission_id: 1, mission_name: 'Mission 1', reserved: true },
+  ];
+
+  const store = mockStore({
+    missions: { missions, isLoading: false, error: undefined },
+  });
+
+  render(
+    <Provider store={store}>
+      <Missions />
+    </Provider>,
+  );
+
+  const leaveButtonElement = screen.getByText(/Leave Mission/i);
+  fireEvent.click(leaveButtonElement);
+
+  expect(store.getActions()).toEqual([
+    { type: 'missions/leaveMission/pending' },
+  ]);
 });
