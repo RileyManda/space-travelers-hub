b220db44f9ea4729ed6c998f045a2bb54b52cc98
RileyManda <rileymanda69@gmail.com>
2023-08-03 18:49:23 +0200

    Implemented missions test

--- src/test/missions.test.js
+++ src/test/missions.test.js
@@ -4,19 +4,44 @@ import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
 import Missions from '../components/Missions';
 
+// import { joinMission, leaveMission } from '../redux/missions/missionsSlice';
+jest.mock('axios', () => ({
+  get: jest.fn(() => Promise.resolve({ data: [] })),
+}));
 const mockStore = configureStore([]);
 
-test('renders loading text when data is loading', () => {
-  const store = mockStore({
-    missions: { missions: [], isLoading: true, error: null },
+describe('Missions Component', () => {
+  it('should display the buttons', () => {
+    const store = mockStore({
+      missions: {
+        missions: [
+          {
+            mission_id: 1,
+            mission_name: 'Mission 1',
+            description: 'Description of Mission 1',
+            reserved: false,
+          },
+          {
+            mission_id: 2,
+            mission_name: 'Mission 2',
+            description: 'Description of Mission 2',
+            reserved: true,
+          },
+        ],
+        isLoading: false,
+        error: null,
+      },
+    });
+
+    render(
+      <Provider store={store}>
+        <Missions />
+      </Provider>,
+    );
+
+    const joinButton = screen.getByRole('button', { name: 'Join Mission' });
+    const leaveButton = screen.getByRole('button', { name: 'Leave Mission' });
+    expect(joinButton).toBeInTheDocument();
+    expect(leaveButton).toBeInTheDocument();
   });
-
-  render(
-    <Provider store={store}>
-      <Missions />
-    </Provider>,
-  );
-
-  const loadingElement = screen.getByText(/Loading.../i);
-  expect(loadingElement).toBeInTheDocument();
 });
