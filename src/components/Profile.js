afd521cfba45db501e6875dd5e53e73fe197832e
RileyManda <rileymanda69@gmail.com>
2023-08-03 13:59:34 +0200

    Implemented mission profile table structure

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,6 +1,13 @@
 import React from 'react';
+import ListGroup from 'react-bootstrap/ListGroup';
 
 const Profile = () => (
-  <div>MyProfile</div>
+  <div>
+    <ListGroup>
+      <ListGroup.Item>Mission1</ListGroup.Item>
+      <ListGroup.Item>Mission2</ListGroup.Item>
+    </ListGroup>
+
+  </div>
 );
 export default Profile;
