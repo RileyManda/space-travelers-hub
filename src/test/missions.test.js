7df120afc749aedc8f615f8656a6abccc282de22
RileyManda <rileymanda69@gmail.com>
2023-08-03 18:11:51 +0200

    Added jest config

--- src/test/missions.test.js
+++ src/test/missions.test.js
@@ -3,7 +3,6 @@ import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
 import Missions from '../components/Missions';
-// import { joinMission, leaveMission } from '../redux/missions/missionsSlice';
 
 const mockStore = configureStore([]);
 
